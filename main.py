ce15337de82aebfea813612b6e64b64c029b2a52
Алина Шириханова <alina.meller.98@mail.ru>
2022-11-09 13:23:58 +0300

    created a window

---
+++ main.py
@@ -0,0 +1,15 @@
+# imports
+from tkinter import *
+import os
+from PIL import Image, ImageTk
+
+# Создание окна
+main = Tk()  # создаем корневой объект - окно
+main.title('Banking System')  # устанавливаем заголовок окна
+
+main.geometry("500x500")  # устанавливаем размеры окна
+
+main.mainloop() # запускаем цикл обработки событий окна для взаимодействия с пользователем
+
+
+
